21698357c47fd23d32dca53896bdf7354a3c7bfb
taqi <taqiedits@gmail.com>
2022-02-09 18:54:25 +0100

    done

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Firma;
+using System.Collections.ObjectModel;
+
+namespace Firma_okienkowo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool checkowanie=true;
+        public ObservableCollection<CzlonekZespolu> Czlonek { get; set; }
+        public Zespol zespol = new Zespol();
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            zespol = (Zespol)Zespol.odczytajXML("zapisz.XML");
+            listboxCzlonkowie.ItemsSource = new ObservableCollection<CzlonekZespolu>(zespol.Czlonkowie);
+            txtNazwa.Text = zespol.Nazwa;
+            txtKierownik.Text = zespol.Kierownik.ToString();
+            
+        }
+       
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            CzlonekZespolu cz = new CzlonekZespolu();
+            OsobaWindow okno = new OsobaWindow(cz);
+            
+            okno.ShowDialog();
+            if (okno.DialogResult == true)
+            {
+                zespol.dodajCzlonka(cz); //dodajemy członka
+                listboxCzlonkowie.ItemsSource = new ObservableCollection<CzlonekZespolu>(zespol.Czlonkowie);
+                checkowanie = false;
+            }
+
+        }
+
+        private void txtCzlonkowieZespolu_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            checkowanie = false;
+        }
+
+        private void buttonZmien_Click(object sender, RoutedEventArgs e)
+        {
+            OsobaWindow okno = new OsobaWindow(zespol.Kierownik as KierownikZespolu);
+            okno.ShowDialog();
+            if (okno.DialogResult == true) { txtKierownik.Text = zespol.Kierownik.ToString(); checkowanie = false; }
+        }
+
+        private void buttonUsun_Click(object sender, RoutedEventArgs e)
+        {
+            int zaznaczony = listboxCzlonkowie.SelectedIndex;
+            if (zaznaczony != -1)
+            {
+                zespol.Czlonkowie.RemoveAt(zaznaczony);
+                listboxCzlonkowie.ItemsSource = new ObservableCollection<CzlonekZespolu>(zespol.Czlonkowie);
+                checkowanie = false;
+            }
+            else
+            {
+                MessageBox.Show("Proszę zaznaczyć członka");
+            }
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void MenuZapisz_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                zespol.Nazwa = txtNazwa.Text;
+                Zespol.zapiszXML(filename,zespol);
+            }
+            checkowanie = true;
+        }
+        private void MenuOtworz_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                zespol = (Zespol)Zespol.odczytajXML(filename);
+                listboxCzlonkowie.ItemsSource = new ObservableCollection<CzlonekZespolu>(zespol.Czlonkowie);
+                txtNazwa.Text = zespol.Nazwa;
+                txtKierownik.Text = zespol.Kierownik.ToString();
+
+            }
+            
+
+        }
+        private void MenuWyjdz_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (checkowanie == false)
+            {
+                if(MessageBox.Show("Czy nie chciałbyś najpierw zapisać zmian?","zamknięcie",MessageBoxButton.YesNo,MessageBoxImage.Information) == MessageBoxResult.No)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    InitializeComponent();
+                    listboxCzlonkowie.ItemsSource = new ObservableCollection<CzlonekZespolu>(zespol.Czlonkowie);
+                    txtNazwa.Text = zespol.Nazwa;
+                    txtKierownik.Text = zespol.Kierownik.ToString();
+                }
+                
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+            
+        }
+
+        private void buttonZmienianie_Click(object sender, RoutedEventArgs e)
+        {
+            int zaznaczony = listboxCzlonkowie.SelectedIndex;
+            if (zaznaczony != -1){
+                OsobaWindow okno = new OsobaWindow(listboxCzlonkowie.SelectedItem as CzlonekZespolu);
+                okno.ShowDialog();
+                if (okno.DialogResult == true)
+                {
+                    InitializeComponent();
+                    listboxCzlonkowie.ItemsSource = new ObservableCollection<CzlonekZespolu>(zespol.Czlonkowie);
+                    txtNazwa.Text = zespol.Nazwa;
+                    txtKierownik.Text = zespol.Kierownik.ToString();
+                    checkowanie = false;
+
+                }
+            }
+            else { MessageBox.Show("Zaznacz jednego z członków!!"); }
+            
+
+        }
+    }
+}
---
+++ OsobaWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using Firma;
+using System.Globalization;
+
+namespace Firma_okienkowo
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OsobaWindow.xaml
+    /// </summary>
+    public partial class OsobaWindow : Window
+    {
+        private Osoba _osoba;
+        private KierownikZespolu _kierownik;
+        private CzlonekZespolu _czlonek;
+        public bool cosiek;
+        public OsobaWindow()
+        {
+            InitializeComponent();
+            comboBoxPlec.Items.Add("mężczyzna");
+            comboBoxPlec.Items.Add("kobieta");
+        }
+        public OsobaWindow(KierownikZespolu osoba):this()
+        {
+            cosiek = true;
+            _osoba = osoba;
+            _kierownik = osoba;
+            lblStanowisko.Content = "Doświadczenie";
+            txtPesel.Text = osoba.pesel;
+            txtName.Text = osoba.Imie;
+            txtLastName.Text = osoba.Nazwisko;
+            txtDateBirth.Text = osoba.DataUrodzenia.ToString("d");
+            comboBoxPlec.Text = ((osoba.plec) == Plcie.M) ? "mężczyzna" : "kobieta";
+            txtStanowisko.Text = osoba.doswiadczenie.ToString();
+            
+            
+            
+        }
+        public OsobaWindow(CzlonekZespolu osoba) : this()
+        {
+            cosiek = false;
+            _osoba = osoba;
+            _czlonek = osoba;
+            lblStanowisko.Content = "Stanowisko";
+            txtPesel.Text = osoba.pesel;
+            txtName.Text = osoba.Imie;
+            txtLastName.Text = osoba.Nazwisko;
+            txtDateBirth.Text = osoba.DataUrodzenia.ToString("d");
+            comboBoxPlec.Text = ((osoba.plec) == Plcie.M) ? "mężczyzna" : "kobieta";
+            txtStanowisko.Text = osoba.funkcja;
+            
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonAccept_Click(object sender, RoutedEventArgs e)
+        {
+            bool checki = false;
+            if (txtPesel.Text != "" && txtName.Text != "" && txtLastName.Text != "")
+            {
+                _osoba.pesel = txtPesel.Text;
+                _osoba.Imie = txtName.Text;
+                _osoba.Nazwisko = txtLastName.Text;
+                string[] fdate = { "yyyy-MM-dd", "yyyy/MM/dd", "MM/dd/yy", "dd-MM-yyyy","dd.MM.yyyy" };
+                if (DateTime.TryParseExact(txtDateBirth.Text, fdate, null, DateTimeStyles.None, out DateTime
+                date) == false)
+                {
+                    MessageBox.Show("Źle wpisana data ur.. Możliwe typy to: yyyy - MM - dd, yyyy / MM / dd,MM / dd / yy, dd - MM - yyyy,dd.MM.yyyy");
+                    checki = false;
+                    InitializeComponent();
+                    
+                }
+                else
+                {
+                    DateTime.TryParseExact(txtDateBirth.Text, fdate, null, DateTimeStyles.None, out DateTime datesmth);
+                    _osoba.DataUrodzenia = datesmth;
+                    _osoba.plec = (comboBoxPlec.Text == "kobieta") ? Plcie.K : Plcie.M;
+                    if(cosiek == true)
+                    {
+                        _kierownik.doswiadczenie = Int32.Parse(txtStanowisko.Text);
+                    }
+                    else if(cosiek == false)
+                    {
+                        _czlonek.funkcja = txtStanowisko.Text;
+                    }
+                    checki = true;
+                }
+            }
+            DialogResult = checki;
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+    
+}
